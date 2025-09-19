4607400ec6eadfa958c6bc07fea92cef61562bf5
igor <160357185+1kookieh@users.noreply.github.com>
2025-09-18 21:04:13 -0300

    Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,81 @@
+import java.util.Random;
+
 public class Main {
-        public static void main(String[] args) {
-            // Criar treinadores
-            Treinador ash = new Treinador("Ash", 15);
-            Treinador misty = new Treinador("Misty", 16);
+    public static void main(String[] args) {
+        Random random = new Random();
 
-            // Criar pokémons
-            Pokemon pikachu = new Pokemon("Pikachu", "Elétrico", 5, 100);
-            Pokemon staryu = new Pokemon("Staryu", "Água", 5, 100);
+        // Criar treinadores
+        Treinador ash = new Treinador("Ash", 15);
+        Treinador misty = new Treinador("Misty", 16);
 
-            // Criar habilidades
-            Habilidade choqueDoTrovao = new Habilidade("Choque do Trovão", 20, "Elétrico");
-            Habilidade jatoDeAgua = new Habilidade("Jato de Água", 15, "Água");
+        // Criar pokémons
+        Pokemon pikachu = new Pokemon("Pikachu", "Elétrico", 5, 100);
+        Pokemon staryu = new Pokemon("Staryu", "Água", 5, 100);
 
-            pikachu.adicionarHabilidade(choqueDoTrovao);
-            staryu.adicionarHabilidade(jatoDeAgua);
+        // Criar habilidades
+        Habilidade choqueDoTrovao = new Habilidade("Choque do Trovão", 20, "Elétrico");
+        Habilidade ataqueRapido = new Habilidade("Ataque Rápido", 10, "Normal");
 
-            // Capturar Pokémons
-            ash.capturarPokemon(pikachu);
-            misty.capturarPokemon(staryu);
+        Habilidade jatoDeAgua = new Habilidade("Jato de Água", 15, "Água");
+        Habilidade chicoteDeBolhas = new Habilidade("Chicote de Bolhas", 12, "Água");
 
-            // Listar Pokémons
-            ash.listarPokemons();
-            misty.listarPokemons();
+        // Adicionar habilidades
+        pikachu.adicionarHabilidade(choqueDoTrovao);
+        pikachu.adicionarHabilidade(ataqueRapido);
 
-            // Usar itens
-            ItemCura pocao = new ItemCura("Poção", "Recupera vida", 30);
-            ItemEvolucao pedraTrovão = new ItemEvolucao("Pedra Trovão", "Evolui Pikachu");
+        staryu.adicionarHabilidade(jatoDeAgua);
+        staryu.adicionarHabilidade(chicoteDeBolhas);
 
-            pocao.aplicar(pikachu);
-            pedraTrovão.aplicar(pikachu);
+        // Capturar Pokémons
+        ash.capturarPokemon(pikachu);
+        misty.capturarPokemon(staryu);
 
-            // Simular batalha
-            Batalha batalha = new Batalha(ash, misty);
-            batalha.iniciar();
+        // Listar Pokémons
+        ash.listarPokemons();
+        misty.listarPokemons();
 
-            batalha.registrarTurno(pikachu, choqueDoTrovao, staryu);
-            batalha.registrarTurno(staryu, jatoDeAgua, pikachu);
+        // Usar itens antes da batalha
+        ItemCura pocao = new ItemCura("Poção", "Recupera vida", 30);
+        ItemEvolucao pedraTrovão = new ItemEvolucao("Pedra Trovão", "Evolui Pikachu");
 
-            System.out.println("Fim da batalha!");
+        pocao.aplicar(pikachu);
+        pedraTrovão.aplicar(pikachu);
+
+        // Iniciar batalha
+        Batalha batalha = new Batalha(ash, misty);
+        batalha.iniciar();
+
+        Pokemon p1 = ash.getPokemons().get(0);
+        Pokemon p2 = misty.getPokemons().get(0);
+
+        // Loop da batalha até um Pokémon ser derrotado
+        int turno = 1;
+        while (p1.getVida() > 0 && p2.getVida() > 0) {
+            System.out.println("\n--- Turno " + turno + " ---");
+
+            // Pikachu ataca
+            Habilidade habP1 = p1.getHabilidades()
+                    .get(random.nextInt(p1.getHabilidades().size()));
+            batalha.registrarTurno(p1, habP1, p2);
+            if (p2.getVida() <= 0) {
+                System.out.println(p2.getNome() + " foi derrotado!");
+                System.out.println("Vitória de " + ash.getNome() + "!");
+                break;
+            }
+
+            // Staryu ataca
+            Habilidade habP2 = p2.getHabilidades()
+                    .get(random.nextInt(p2.getHabilidades().size()));
+            batalha.registrarTurno(p2, habP2, p1);
+            if (p1.getVida() <= 0) {
+                System.out.println(p1.getNome() + " foi derrotado!");
+                System.out.println("Vitória de " + misty.getNome() + "!");
+                break;
+            }
+
+            turno++;
         }
+
+        System.out.println("\nFim da batalha!");
+    }
 }
